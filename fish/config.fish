568d78b83048996b484cf88e15cb90db1dc1bac4
demikotomi8 <86958565+demikotomi8@users.noreply.github.com>
2026-02-23 07:02:45 -0500

    Delete fish directory

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -gx PATH $HOME/.config/scripts $HOME/go/bin $PATH
-set -x fish_greeting ""
-set -x EDITOR nvim
-set -x PAGER less
-
-fish_vi_key_bindings
-
-zoxide init fish | source
-starship init fish | source
